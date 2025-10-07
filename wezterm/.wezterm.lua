c7761aa971e30e6fdfcfdcea62d4017533a8d55a
Samuel Jackson Gido <132026917+samgido@users.noreply.github.com>
2025-10-07 15:20:01 -0500

    remove dotter folder, add multi platform functionality for wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,6 +2,9 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.default_cwd = '~/Documents'
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+	config.default_prog = { 'pwsh' }
+end
 
 -- Set theme
 config.color_scheme = 'Adventure'
