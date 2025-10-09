8b68a58cfe1aba04944f2f472f4749eb4af2bf08
Samuel Jackson Gido <132026917+samgido@users.noreply.github.com>
2025-10-08 20:12:44 -0500

    change wezterm frontend

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,15 +1,19 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.default_cwd = '~/Documents'
+-- Optimizations
+config.front_end = "WebGpu"
+
+-- Startup behavior
+config.default_cwd = wezterm.home_dir .. '/Documents'
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
 	config.default_prog = { 'pwsh' }
 end
 
--- Set theme
+-- Theme
 config.color_scheme = 'Adventure'
 
--- Set fonts
+-- Fonts
 config.font = wezterm.font 'CodeNewRoman Nerd Font'
 config.font_size = 14
 
